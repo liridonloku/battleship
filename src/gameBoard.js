33421aeb8da9bf29e47c14b09d528904bd5080ea
Liridon Loku <liridonloku@gmail.com>
2021-10-16 13:24:24 +0200

    Add placeShip function to gameboard and write tests for it

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
+import { shipFactory } from "./ship";
+
 /* const shipModel = {
   carrier: 5,
   battleship: 4,
@@ -9,15 +11,30 @@
 }; */
 
 const newBoard = () => {
-  const array = [];
+  const ships = [];
+  const board = [];
   for (let i = 0; i < 10; i += 1) {
     const row = [];
     for (let j = 0; j < 10; j += 1) {
       row.push(`${i}-${j}`);
     }
-    array.push(row);
+    board.push(row);
   }
-  return array;
+  function placeShip(length, position) {
+    if (10 - position.charAt(2) < length) {
+      throw new Error("Can't place a ship of that length there");
+    } else {
+      const coordinates = [];
+      const firstCoordinate = parseInt(position.charAt(0), 10);
+      const secondCoordinate = parseInt(position.charAt(2), 10);
+      for (let i = 0; i < length; i += 1) {
+        coordinates.push(`${firstCoordinate}-${secondCoordinate + i}`);
+      }
+      const newShip = shipFactory(coordinates);
+      ships.push(newShip);
+    }
+  }
+  return { board, ships, placeShip };
 };
 
 export { newBoard };
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -2,5 +2,16 @@ import { newBoard } from "./gameBoard";
 
 test("Create new board", () => {
   const board = newBoard();
-  expect(board[0][0]).toBe("0-0");
+  expect(board.board[0][0]).toBe("0-0");
 });
+
+test("Place ship", () => {
+  const board = newBoard();
+  const board2 = newBoard();
+  board.placeShip(3, "5-5");
+  expect(board.ships[0]).toHaveLength(3);
+  expect(board.ships[0].coordinates).toEqual(["5-5", "5-6", "5-7"]);
+  expect(board2.ships[0]).toBe(undefined);
+});
+
+test("Receive Attack", () => {});
