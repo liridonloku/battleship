a8a96070481856ba72c5dc412ad49d6be7913f2d
Liridon Loku <liridonloku@gmail.com>
2021-10-23 20:51:42 +0200

    Start work on dom.js

---
+++ src/DOM.js
@@ -0,0 +1,10 @@
+const domElements = {
+  player1Name: document.querySelector(".left-content>.player-info"),
+  player1Board: document.querySelector(".left-content>.board-container>.board"),
+  player2Name: document.querySelector(".right-content>.player-info"),
+  player2Board: document.querySelector(
+    ".right-content>.board-container>.board"
+  ),
+};
+
+const loadGameElements = () => {};
